26bc999ad9954094d6b48514f9f862ee67640eb8
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-03 21:27:02 +0200

    Added API key

--- src/components/api.js
+++ src/components/api.js
@@ -1,1 +1,8 @@
 import axios from "axios";
+
+axios.defaults.baseURL = 'https://www.themoviedb.org/';
+const API_KEY = '4707371d53aa0467b337f4fda109b465';
+
+export const getTrending = async () => {
+const response = await axios.get()
+}
