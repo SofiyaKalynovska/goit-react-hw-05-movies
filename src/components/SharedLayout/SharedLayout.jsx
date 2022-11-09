4c4e18697ef4ab6ad46505d1b35f20a42379ee39
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-09 15:52:36 +0200

    Added main navigation

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { Container, Link, Navigation } from "./SharedLayout.styled";
+
+export const SharedLayout = () => {
+  return (
+    <Container>
+        <Navigation>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </Navigation>
+      <Outlet />
+    </Container>
+  );
+};
---
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Container = styled.div`
+  background-color: #f2f2f2;
+`;
+export const Navigation = styled.nav`
+  padding: 10px;
+  display: flex;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+`
+export const Link = styled(NavLink)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-size: 28px;
+  font-weight: 500;
+   &.active {
+    color: red;
+  }
+`;
