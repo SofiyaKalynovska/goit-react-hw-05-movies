666aa163a613cc22a44c85c7e8fc5fdff04a2ef1
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-11-09 15:52:47 +0200

    Added home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,2 +1,49 @@
-// import { getTrending } from "components/api";
+import { fetchTrending } from 'components/api';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import {
+  Title,
+  MoviesList,
+  MovieItem,
+  Poster,
+  MovieTitle,
+} from './Home.styled';
+import { Loading } from 'components/Loading/Loading';
+import { Link } from 'react-router-dom';
 
+export const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function getTrendingMovies() {
+      try {
+        setIsLoading(true);
+        const arrOfMovies = await fetchTrending();
+        setTrendingMovies(prevState => [...prevState, ...arrOfMovies.results]);
+        console.log(arrOfMovies.results);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getTrendingMovies();
+  }, []);
+  return (
+    <>
+      <Title>Trending today</Title>
+      {isLoading && <Loading isLoading={isLoading} />}
+      <MoviesList>
+        {trendingMovies.map(({ id, title, poster_path }) => (
+          <MovieItem key={id}>
+            <Link to={`movies/${id}`}>
+              <MovieTitle>{title}</MovieTitle>
+              <Poster src={`https://image.tmdb.org/t/p/w500/${poster_path}`} />
+            </Link>
+          </MovieItem>
+        ))}
+      </MoviesList>
+    </>
+  );
+};
